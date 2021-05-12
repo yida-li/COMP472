0af5e00c4da9602d980c1e5c013e2e89d749c763
yida-li <gabrielyidali@gmail.com>
2021-05-12 09:01:50 -0400

    hello world

---
+++ practice/00001.py
@@ -0,0 +1,8 @@
+####################################################
+# 1. Hello world & basic input/output
+####################################################
+
+print("This is the first python code ")
+f1 = input("Enter your first initial ")
+f2 = input("Enter your second initial ")
+print("Hello world to :", f1, f2)
