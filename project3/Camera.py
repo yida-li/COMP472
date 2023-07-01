719c9e6c3a2d41a2ce1e5aed106e3fe9cc4e6118
yida-li <gabrielyidali@gmail.com>
2023-07-01 12:22:08 -0400

    added a function to take picture

--- project3/Camera.py
+++ project3/Camera.py
@@ -3,13 +3,23 @@ from time import sleep
 video_capture = cv2.VideoCapture(0)
 
 
+def take_picture(frame):
+    # Save the frame as an image file
+    cv2.imwrite("picture.PNG", frame)
+    print("Picture taken!")
+
+
 while True:
     sleep(0.001)
     ret, frame = video_capture.read()
 
-    if cv2.waitKey(1) & 0xFF == ord('x'):
-        break
+    key = cv2.waitKey(1) & 0xFF
 
+    if key == ord('x'):
+        break
+    elif key == ord('p'):  # Press 'p' to take a picture
+        take_picture(frame)
+    
     cv2.imshow(
         'Welcome to the Simplest Camera App\t\t\t\t\t\t press x to exit ', frame)
 
